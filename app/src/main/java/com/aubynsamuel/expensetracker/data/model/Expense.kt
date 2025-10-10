fff4c93edc3b62ac3412e5b4c6f414733c4ed394
De General <aubynsamuel05@gmail.com>
2025-10-10 15:36:41 +0000

    feat: Refactor navigation using Navigation 3

--- app/src/main/java/com/aubynsamuel/expensetracker/data/model/Expense.kt
+++ app/src/main/java/com/aubynsamuel/expensetracker/data/model/Expense.kt
@@ -2,7 +2,9 @@ package com.aubynsamuel.expensetracker.data.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
 
+@Serializable
 @Entity(tableName = "expenses")
 data class Expense(
     @PrimaryKey(autoGenerate = true)
